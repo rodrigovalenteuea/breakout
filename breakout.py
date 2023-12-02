4c0427330db4e52a4f8037ef6799e806ef0de031
Aglison Balieiro <abds.snf23@uea.edu.br>
2023-12-02 16:38:02 -0400

    paddle and movement

--- breakout.py
+++ breakout.py
@@ -9,6 +9,7 @@ size = (WIDTH, HEIGHT)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 FPS = 60
+bg = (0, 0, 0)
 
 WHITE = (255, 255, 255)
 GREY = (212, 210, 212)
@@ -30,17 +31,19 @@ x_gap = 7
 y_gap = 5
 wall_width = 16
 
-paddle_width = 54
-paddle_height = 20
+paddle = pygame.Rect(WIDTH / 2 - 100, 950, 55, 16)
+p_speed = 10
+
 
 def draw_wall():
-
     pygame.draw.line(screen, GREY, [0, 19], [WIDTH, 19], 40)
     pygame.draw.line(screen, GREY, [(wall_width / 2) - 1, 0], [(wall_width / 2) - 1, HEIGHT], wall_width)
-    pygame.draw.line(screen, GREY, [WIDTH - (wall_width / 2) - 1, 0], [WIDTH - (wall_width / 2) - 1, HEIGHT], wall_width)
+    pygame.draw.line(screen, GREY, [WIDTH - (wall_width / 2) - 1, 0], [WIDTH - (wall_width / 2) - 1, HEIGHT],
+                     wall_width)
 
-    pygame.draw.line(screen, BLUE, [0, 300], [(wall_width-1), 300], wall_width)
-    pygame.draw.line(screen, BLUE, [WIDTH, 300], [(WIDTH-wall_width), 300], wall_width)
+    pygame.draw.line(screen, BLUE, [0, 300], [(wall_width - 1), 300], wall_width)
+    pygame.draw.line(screen, BLUE, [WIDTH, 300], [(WIDTH - wall_width), 300], wall_width)
+
 
 def main(score, balls):
     step = 0
@@ -51,10 +54,17 @@ def main(score, balls):
 
             if event.type == pygame.QUIT:
                 run = False
-
+            key = pygame.key.get_pressed()
+            if key[pygame.K_LEFT] and paddle.left > 0:
+                paddle.left -= p_speed
+            if key[pygame.K_RIGHT] and paddle.right < WIDTH:
+                paddle.right += p_speed
         draw_wall()
         pygame.display.update()
+        screen.fill(bg)
+        pygame.draw.rect(screen, (255, 255, 255), paddle)
         clock.tick(FPS)
     pygame.quit()
+
 
 main(score, balls)
